4171c7bc7b3413127d98638b2e98e89c5310fd33
tsaglash <162251233+tsaglash@users.noreply.github.com>
2024-06-11 11:38:32 +0800

    Tses
    minii hicheel

---
+++ Boox.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+// import emoji from "../components/img/111.";
+import "../App.css";
+
+const Boox = () => {
+  const [emoji, setEmoji] = useState([]);
+  return <div className="booox">Emoji</div>;
+};
+
+export default Boox;
---
+++ Emoji.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./emoji.css";
+const Emoji = () => {
+  return <div className="emoji">hello</div>;
+};
+
+export default Emoji;
